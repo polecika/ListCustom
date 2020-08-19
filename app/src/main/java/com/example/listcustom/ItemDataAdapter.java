041a0a40d5a2e3ab8398a5b86427eb4efd73901e
Shvets <polecika@gmail.com>
2020-08-20 02:18:32 +0300

    Initial commit

---
+++ app/src/main/java/com/example/listcustom/ItemDataAdapter.java
@@ -0,0 +1,96 @@
+package com.example.listcustom;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemDataAdapter extends BaseAdapter {
+    private List<ItemData> items;
+
+
+    private LayoutInflater inflater;
+
+
+    private CompoundButton.OnCheckedChangeListener myCheckChangeList
+            = new CompoundButton.OnCheckedChangeListener() {
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            items.get((Integer) buttonView.getTag()).setChecked(isChecked);
+        }
+    };
+
+
+
+    ItemDataAdapter(Context context, List<ItemData> items) {
+        if (items == null) {
+            this.items = new ArrayList<>();
+        } else {
+            this.items = items;
+        }
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+
+    void addItem(ItemData item) {
+        this.items.add(item);
+        notifyDataSetChanged();
+    }
+
+
+
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+
+
+    @Override
+    public ItemData getItem(int position) {
+        if (position < items.size()) {
+            return items.get(position);
+        } else {
+            return null;
+        }
+    }
+
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        if (view == null) {
+            view = inflater.inflate(R.layout.item_list_view, parent, false);
+        }
+
+        ItemData itemData = items.get(position);
+
+        ImageView image = view.findViewById(R.id.icon);
+        TextView title = view.findViewById(R.id.title);
+        TextView subtitle = view.findViewById(R.id.subtitle);
+        CheckBox checkBox = view.findViewById(R.id.checkbox);
+
+        image.setImageDrawable(itemData.getImage());
+        title.setText(itemData.getTitle());
+        subtitle.setText(itemData.getSubtitle());
+        checkBox.setOnCheckedChangeListener(myCheckChangeList);
+        checkBox.setTag(position);
+        checkBox.setChecked(itemData.isChecked());
+
+        return view;
+    }
+}
+
---
+++ app/src/main/java/com/example/listcustom/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.listcustom;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Random random = new Random();
+
+    private ItemDataAdapter adapter;
+
+    private List<Drawable> images = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        ListView listView = findViewById(R.id.listView);
+
+        setSupportActionBar(toolbar);
+
+        fillImages();
+
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                generateRandomItemData();
+            }
+        });
+
+        adapter = new ItemDataAdapter(this, null);
+        listView.setAdapter(adapter);
+
+    }
+
+    private void fillImages() {
+        images.add(ContextCompat.getDrawable(MainActivity.this,
+                android.R.drawable.ic_menu_report_image));
+        images.add(ContextCompat.getDrawable(MainActivity.this,
+                android.R.drawable.ic_menu_add));
+        images.add(ContextCompat.getDrawable(MainActivity.this,
+                android.R.drawable.ic_menu_agenda));
+        images.add(ContextCompat.getDrawable(MainActivity.this,
+                android.R.drawable.ic_menu_camera));
+        images.add(ContextCompat.getDrawable(MainActivity.this,
+                android.R.drawable.ic_menu_call));
+    }
+
+
+    private void generateRandomItemData() {
+        adapter.addItem(new ItemData(
+                images.get(random.nextInt(images.size())),
+                "Hello" + adapter.getCount(),
+                "It\'s me",
+                random.nextBoolean()));
+    }
+}
